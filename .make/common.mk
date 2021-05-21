f38972ae4a3af575527d699e0391356b9e256a5a
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2021-05-21 20:05:47 +0000

    neighbour: fix a race in neigh_destroy()

--- .make/common.mk
+++ .make/common.mk
@@ -13,4 +13,13 @@ endif
 HAS_GIT := $(shell command -v git 2> /dev/null)
 
 ifdef HAS_GIT
-	## Do 
+	## Do we have a repo?
+	HAS_REPO := $(shell git rev-parse --is-inside-work-tree 2> /dev/null)
+	ifdef HAS_REPO
+		## Automatically detect the repo owner and repo name (for local use with Git)
+		REPO_NAME=$(shell basename "$(shell git rev-parse --show-toplevel 2> /dev/null)")
+		OWNER=$(shell git config --get remote.origin.url | sed 's/git@$(GIT_DOMAIN)://g' | sed 's/\/$(REPO_NAME).git//g')
+		REPO_OWNER=$(shell echo $(OWNER) | tr A-Z a-z)
+		VERSION_SHORT=$(shell git describe --tags --always --abbrev=0)
+		export REPO_NAME, REPO_OWNER, VERSION_SHORT
+	endif
