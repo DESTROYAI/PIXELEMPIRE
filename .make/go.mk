188ee14fc53825a4eb7811b214d102394d3d8822
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2021-05-22 14:58:34 +0000

    PDFBOX-4071: improve javadoc

--- .make/go.mk
+++ .make/go.mk
@@ -11,4 +11,25 @@ endif
 ## Set the binary release names
 DARWIN=$(BINARY_NAME)-darwin
 LINUX=$(BINARY_NAME)-linux
-W
+WINDOWS=$(BINARY_NAME)-windows.exe
+
+.PHONY: test lint vet install
+
+bench:  ## Run all benchmarks in the Go application
+	@go test -bench=. -benchmem
+
+build-go:  ## Build the Go application (locally)
+	@go build -o bin/$(BINARY_NAME)
+
+clean-mods: ## Remove all the Go mod cache
+	@go clean -modcache
+
+coverage: ## Shows the test coverage
+	@go test -coverprofile=coverage.out ./... && go tool cover -func=coverage.out
+
+godocs: ## Sync the latest tag with GoDocs
+	@test $(GIT_DOMAIN)
+	@test $(REPO_OWNER)
+	@test $(REPO_NAME)
+	@test $(VERSION_SHORT)
+	@curl https://proxy.golan
