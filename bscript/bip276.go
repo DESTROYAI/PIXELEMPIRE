f19e8284887e8fb7d404acf627ad7c3f5a63a0c7
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2021-05-28 07:28:43 +0000

    Whitespaces.

--- bscript/bip276.go
+++ bscript/bip276.go
@@ -22,4 +22,23 @@ type BIP276 struct {
 // specifies if it is a script or template.
 const PrefixScript = "bitcoin-script"
 
-// PrefixTemplate is the prefix in the BIP276
+// PrefixTemplate is the prefix in the BIP276 standard which
+// specifies if it is a script or template.
+const PrefixTemplate = "bitcoin-template"
+
+// CurrentVersion provides the ability to
+// update the structure of the data that
+// follows it.
+const CurrentVersion = 1
+
+// NetworkMainnet specifies that the data is only
+// valid for use on the main network.
+const NetworkMainnet = 1
+
+// NetworkTestnet specifies that the data is only
+// valid for use on the test network.
+const NetworkTestnet = 2
+
+var validBIP276 = regexp.MustCompile(`^(.+?):(\d{2})(\d{2})([0-9A-Fa-f]+)([0-9A-Fa-f]{8})$`)
+
+// EncodeBIP276 is used to encode specific (non-
