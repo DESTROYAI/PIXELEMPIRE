4349f62b3a94f0de16a0f1059bb83e6a898afbb4
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2021-05-29 22:32:57 +0000

    merging with mysql-5.1-bugteam

--- bscript/interpreter/config.go
+++ bscript/interpreter/config.go
@@ -7,4 +7,27 @@ type config interface {
 	MaxOps() int
 	MaxStackSize() int
 	MaxScriptSize() int
-	MaxScriptE
+	MaxScriptElementSize() int
+	MaxScriptNumberLength() int
+	MaxPubKeysPerMultiSig() int
+}
+
+// Limits applied to transactions before genesis
+const (
+	MaxOpsBeforeGenesis                = 500
+	MaxStackSizeBeforeGenesis          = 1000
+	MaxScriptSizeBeforeGenesis         = 10000
+	MaxScriptElementSizeBeforeGenesis  = 520
+	MaxScriptNumberLengthBeforeGenesis = 4
+	MaxPubKeysPerMultiSigBeforeGenesis = 20
+)
+
+type beforeGenesisConfig struct{}
+type afterGenesisConfig struct{}
+
+func (a *afterGenesisConfig) AfterGenesis() bool {
+	return true
+}
+
+func (b *beforeGenesisConfig) AfterGenesis() bool {
+	return fals
