f8e9c8c690070c7ec31580c83e70468b32292afb
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2021-09-26 20:51:03 +0000

    Update on graph

--- bscript/interpreter/debug/example_test.go
+++ bscript/interpreter/debug/example_test.go
@@ -26,4 +26,24 @@ func ExampleDebugger_AfterStep() {
 	debugger := debug.NewDebugger()
 	debugger.AttachAfterStep(func(state *interpreter.State) {
 		frames := make([]string, len(state.DataStack))
-		for i, frame :=
+		for i, frame := range state.DataStack {
+			frames[i] = hex.EncodeToString(frame)
+		}
+		fmt.Println(strings.Join(frames, "|"))
+	})
+
+	if err := interpreter.NewEngine().Execute(
+		interpreter.WithScripts(lockingScript, unlockingScript),
+		interpreter.WithAfterGenesis(),
+		interpreter.WithDebugger(debugger),
+	); err != nil {
+		fmt.Println(err)
+	}
+
+	// Output:
+	// 68656c6c6f
+	// 68656c6c6f|777f726c64
+	// 777f726c64|68656c6c6f
+	// 777f726c6468656c6c6f
+	// 8a0e597fd66749ca1a2f098f4ef706422c63a96dceef4abfd74517b10cd12f63
+	// 8a0e597fd66749ca1a2f098f4ef706422c63a96dceef4abfd74517b10cd12f63|8376118fc
