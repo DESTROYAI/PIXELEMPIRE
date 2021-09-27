af28f75f939754300f6ed0d0f0d2fbce557b33e5
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2021-09-27 10:23:46 +0000

    Improve error message

--- bscript/interpreter/engine_test.go
+++ bscript/interpreter/engine_test.go
@@ -14,3 +14,36 @@ import (
 	"github.com/libsv/go-bt/v2/bscript/interpreter/scriptflag"
 	"github.com/libsv/go-bt/v2/sighash"
 	"github.com/stretchr/testify/assert"
+)
+
+// TestBadPC sets the pc to a deliberately bad result then confirms that Step()
+// and Disasm fail correctly.
+func TestBadPC(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		script, off int
+	}{
+		{script: 2, off: 0},
+		{script: 0, off: 2},
+	}
+
+	uscript, err := bscript.NewFromASM("OP_NOP")
+	if err != nil {
+		t.Errorf("failed to create unlocking script %e", err)
+	}
+
+	tx := &bt.Tx{
+		Version: 1,
+		Inputs: []*bt.Input{{
+			PreviousTxOutIndex: 0,
+			UnlockingScript:    uscript,
+			SequenceNumber:     4294967295,
+		}},
+		Outputs: []*bt.Output{{
+			Satoshis: 1000000000,
+		}},
+		LockTime: 0,
+	}
+
+	lsc
