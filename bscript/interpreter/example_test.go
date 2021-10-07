ed867e8d8a882974c6a96efbe1f646a9e8b0850c
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2021-10-07 19:08:30 +0000

    add score

---
+++ bscript/interpreter/example_test.go
@@ -0,0 +1,13 @@
+package interpreter_test
+
+import (
+	"fmt"
+
+	"context"
+
+	"github.com/libsv/go-bt/v2"
+	"github.com/libsv/go-bt/v2/bscript/interpreter"
+	"golang.org/x/sync/errgroup"
+)
+
+func Exam
