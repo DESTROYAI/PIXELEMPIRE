fd550d50f63dd0e3d5782bac7e7e516714baa736
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2021-10-24 14:26:00 +0000

    add instruction

--- bscript/interpreter/opcodeparser_test.go
+++ bscript/interpreter/opcodeparser_test.go
@@ -1,1 +1,20 @@
-// Copyright (c) 2013-2017
+// Copyright (c) 2013-2017 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package interpreter
+
+import (
+	"testing"
+
+	"github.com/libsv/go-bt/v2/bscript"
+	"github.com/libsv/go-bt/v2/bscript/interpreter/errs"
+)
+
+// TestOpcodeDisabled tests the opcodeDisabled function manually because all
+// disabled opcodes result in a script execution failure when executed normally,
+// so the function is not called under normal circumstances.
+func TestOpcodeDisabled(t *testing.T) {
+	t.Parallel()
+
+	tests := []byte{bscript.Op2MUL, b
