637d3d67dec5b9ac07ecf6ed7d13078f64d9a192
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2021-10-24 22:49:06 +0000

    Update mark.md

--- bscript/interpreter/operations.go
+++ bscript/interpreter/operations.go
@@ -10,4 +10,30 @@ import (
 	"github.com/libsv/go-bk/bec"
 	"github.com/libsv/go-bk/crypto"
 	"github.com/libsv/go-bt/v2"
-	"github.com/libsv/go-
+	"github.com/libsv/go-bt/v2/bscript"
+	"github.com/libsv/go-bt/v2/bscript/interpreter/errs"
+	"github.com/libsv/go-bt/v2/bscript/interpreter/scriptflag"
+	"github.com/libsv/go-bt/v2/sighash"
+	"golang.org/x/crypto/ripemd160"
+)
+
+// Conditional execution constants.
+const (
+	opCondFalse = 0
+	opCondTrue  = 1
+	opCondSkip  = 2
+)
+
+type opcode struct {
+	val    byte
+	name   string
+	length int
+	exec   func(*ParsedOpcode, *thread) error
+}
+
+func (o opcode) Name() string {
+	return o.name
+}
+
+// opcodeArray associates an opcode with its respective function, and defines them in order as to
+// be corre
