bcb316f029a4a36cca542542860d2c1398254622
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-04-02 12:58:54 +0000

    language tweaks

--- bscript/interpreter/operations.go
+++ bscript/interpreter/operations.go
@@ -1484,4 +1484,31 @@ func opcodeRShift(op *ParsedOpcode, t *thread) error {
 // Stack transformation (x1==0, x2==0): [... 0 0] -> [... 0]
 // Stack transformation (x1!=0, x2==0): [... 5 0] -> [... 0]
 // Stack transformation (x1==0, x2!=0): [... 0 7] -> [... 0]
-// Stack transformation (x1!=0, x2!=
+// Stack transformation (x1!=0, x2!=0): [... 4 8] -> [... 1]
+func opcodeBoolAnd(op *ParsedOpcode, t *thread) error {
+	v0, err := t.dstack.PopInt()
+	if err != nil {
+		return err
+	}
+
+	v1, err := t.dstack.PopInt()
+	if err != nil {
+		return err
+	}
+
+	var n int64
+	if !v0.IsZero() && !v1.IsZero() {
+		n = 1
+	}
+
+	t.dstack.PushInt(&scriptNumber{
+		val:          big.NewInt(n),
+		afterGenesis: t.afterGenesis,
+	})
+	return nil
+}
+
+// opcodeBoolOr treats the top two items on the data stack as integers.  When
+// either of them are not zero, they are replaced with a 1, otherwise a 0.
+//
+// Stack transformation (x1==0, x2==0): [... 0 
