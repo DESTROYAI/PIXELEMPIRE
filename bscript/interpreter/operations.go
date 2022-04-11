cf5d9f0f67164a8093935abc0589b7120d0004e4
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-04-11 11:34:28 +0000

    new tool

--- bscript/interpreter/operations.go
+++ bscript/interpreter/operations.go
@@ -1660,4 +1660,27 @@ func opcodeGreaterThan(op *ParsedOpcode, t *thread) error {
 	}
 
 	t.dstack.PushInt(&scriptNumber{
-		val:    
+		val:          big.NewInt(n),
+		afterGenesis: t.afterGenesis,
+	})
+	return nil
+}
+
+// opcodeLessThanOrEqual treats the top two items on the data stack as integers.
+// When the second-to-top item is less than or equal to the top item, they are
+// replaced with a 1, otherwise a 0.
+//
+// Stack transformation: [... x1 x2] -> [... bool]
+func opcodeLessThanOrEqual(op *ParsedOpcode, t *thread) error {
+	v0, err := t.dstack.PopInt()
+	if err != nil {
+		return err
+	}
+
+	v1, err := t.dstack.PopInt()
+	if err != nil {
+		return err
+	}
+
+	var n int64
+	if v1.LessThanOrE
