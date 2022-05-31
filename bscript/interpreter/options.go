cff4a610b767692c46c614598172e2b85f34110b
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-05-31 16:00:33 +0000

    Created .gitignore

--- bscript/interpreter/options.go
+++ bscript/interpreter/options.go
@@ -19,4 +19,25 @@ func WithTx(tx *bt.Tx, inputIdx int, prevOutput *bt.Output) ExecutionOptionFunc 
 }
 
 // WithScripts configure the execution to run again a set of *bscript.Script.
-func WithScripts(lockingScript *bscript.Script, unlockingScript 
+func WithScripts(lockingScript *bscript.Script, unlockingScript *bscript.Script) ExecutionOptionFunc {
+	return func(p *execOpts) {
+		p.lockingScript = lockingScript
+		p.unlockingScript = unlockingScript
+	}
+}
+
+// WithAfterGenesis configure the execution to operate in an after-genesis context.
+func WithAfterGenesis() ExecutionOptionFunc {
+	return func(p *execOpts) {
+		p.flags.AddFlag(scriptflag.UTXOAfterGenesis)
+	}
+}
+
+// WithForkID configure the execution to allow a tx with a fork id.
+func WithForkID() ExecutionOptionFunc {
+	return func(p *execOpts) {
+		p.flags.AddFlag(scriptflag.EnableSighashForkID)
+	}
+}
+
+// WithP2SH
