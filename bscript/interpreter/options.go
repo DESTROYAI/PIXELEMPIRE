8fe19e542e410b15292fadc34bfd2ae40fadbbb2
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-05-31 06:38:18 +0000

    More fixing

--- bscript/interpreter/options.go
+++ bscript/interpreter/options.go
@@ -2,4 +2,21 @@ package interpreter
 
 import (
 	"github.com/libsv/go-bt/v2"
-	"github.com/libsv/go-bt/v2/
+	"github.com/libsv/go-bt/v2/bscript"
+	"github.com/libsv/go-bt/v2/bscript/interpreter/scriptflag"
+)
+
+// ExecutionOptionFunc for setting execution options.
+type ExecutionOptionFunc func(p *execOpts)
+
+// WithTx configure the execution to run again a tx.
+func WithTx(tx *bt.Tx, inputIdx int, prevOutput *bt.Output) ExecutionOptionFunc {
+	return func(p *execOpts) {
+		p.tx = tx
+		p.previousTxOut = prevOutput
+		p.inputIdx = inputIdx
+	}
+}
+
+// WithScripts configure the execution to run again a set of *bscript.Script.
+func WithScripts(lockingScript *bscript.Script, unlockingScript 
