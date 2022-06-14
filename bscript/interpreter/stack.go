e71e3b077ffe82a38d7b8ed65a8034ee82c946fc
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-06-14 11:02:13 +0000

    modified footer

--- bscript/interpreter/stack.go
+++ bscript/interpreter/stack.go
@@ -1,3 +1,35 @@
 // Copyright (c) 2013-2017 The btcsuite developers
 // Use of this source code is governed by an ISC
-/
+// license that can be found in the LICENSE file.
+
+package interpreter
+
+import (
+	"encoding/hex"
+
+	"github.com/libsv/go-bt/v2/bscript/interpreter/errs"
+)
+
+// asBool gets the boolean value of the byte array.
+func asBool(t []byte) bool {
+	for i := range t {
+		if t[i] != 0 {
+			// Negative 0 is also considered false.
+			if i == len(t)-1 && t[i] == 0x80 {
+				return false
+			}
+			return true
+		}
+	}
+	return false
+}
+
+// fromBool converts a boolean into the appropriate byte array.
+func fromBool(v bool) []byte {
+	if v {
+		return []byte{1}
+	}
+	return nil
+}
+
+// stack represents 
