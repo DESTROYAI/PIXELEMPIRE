e370842cb9c672b4b044f3ba0ef22f05209b737d
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-07-25 22:42:05 +0000

    Prepared for release

--- bscript/interpreter/stack_test.go
+++ bscript/interpreter/stack_test.go
@@ -17,4 +17,21 @@ import (
 
 // tstCheckScriptError ensures the type of the two passed errors are of the
 // same type (either both nil or both of type Error) and their error codes
-// match
+// match when not nil.
+func tstCheckScriptError(gotErr, wantErr error) error {
+	// Ensure the error code is of the expected type and the error
+	// code matches the value specified in the test instance.
+	if reflect.TypeOf(gotErr) != reflect.TypeOf(wantErr) {
+		return fmt.Errorf("wrong error - got %T (%[1]v), want %T", gotErr, wantErr) //nolint:errorlint // test code
+	}
+	if gotErr == nil {
+		return nil
+	}
+
+	// Ensure the want error type is a script error.
+	werr := &errs.Error{}
+	if ok := errors.As(wantErr, werr); !ok {
+		return fmt.Errorf("unexpected test error type %T", wantErr) //nolint:errorlint // test code
+	}
+
+	// Ensure the err
