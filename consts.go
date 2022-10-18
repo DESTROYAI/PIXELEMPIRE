f3762dea10ceaa1570351b4f399785ef43738619
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-10-18 21:31:25 +0000

    ALSA: hda - Don't access stereo amps for mono channel widgets

--- consts.go
+++ consts.go
@@ -5,4 +5,19 @@ const (
 	// of a transaction input can be.
 	MaxTxInSequenceNum uint32 = 0xffffffff
 
-	// MaxPrevOutIndex 
+	// MaxPrevOutIndex is the maximum index the index field of a previous
+	// outpoint can be.
+	MaxPrevOutIndex uint32 = 0xffffffff
+
+	// SequenceLockTimeDisabled is a flag that if set on a transaction
+	// input's sequence number, the sequence number will not be interpreted
+	// as a relative locktime.
+	SequenceLockTimeDisabled = 1 << 31
+
+	// SequenceLockTimeIsSeconds is a flag that if set on a transaction
+	// input's sequence number, the relative locktime has units of 512
+	// seconds.
+	SequenceLockTimeIsSeconds = 1 << 22
+
+	// SequenceLockTimeMask is a mask that extracts the relative locktime
+	// when masked 
