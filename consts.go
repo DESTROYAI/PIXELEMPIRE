71883f05834018a692eb1a1ca3cb90aae1c1c90d
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-10-18 17:36:14 +0000

    Saved README.md with Dillinger.io

---
+++ consts.go
@@ -0,0 +1,8 @@
+package bt
+
+const (
+	// MaxTxInSequenceNum is the maximum sequence number the sequence field
+	// of a transaction input can be.
+	MaxTxInSequenceNum uint32 = 0xffffffff
+
+	// MaxPrevOutIndex 
