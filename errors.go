1888d1cf38992ba9d323f9bf92cc194824ba5d9e
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-10-22 11:09:36 +0000

    Release v1.6.2

--- errors.go
+++ errors.go
@@ -8,4 +8,18 @@ var (
 	ErrTxNil             = errors.New("tx is nil")
 	ErrTxTooShort        = errors.New("too short to be a tx - even an empty tx has 10 bytes")
 	ErrNLockTimeLength   = errors.New("nLockTime length must be 4 bytes long")
-	ErrEmptyValues       = erro
+	ErrEmptyValues       = errors.New("empty value or values passed, all arguments are required and cannot be empty")
+	ErrUnsupportedScript = errors.New("non-P2PKH input used in the tx - unsupported")
+	ErrInvalidScriptType = errors.New("invalid script type")
+	ErrNoUnlocker        = errors.New("unlocker not supplied")
+)
+
+// Sentinal errors reported by inputs.
+var (
+	ErrInputNoExist  = errors.New("specified input does not exist")
+	ErrInputTooShort = errors.New("input length too short")
+)
+
+// Sentinal errors reported by outputs.
+var (
+	ErrOutputNoExist  = errors.New("specified output do
