218c22c8e071b076f825875a73b1487d625e8447
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-10-23 08:15:01 +0000

    2.4.3

--- examples/create_tx/create_tx.go
+++ examples/create_tx/create_tx.go
@@ -4,4 +4,26 @@ import (
 	"context"
 	"log"
 
-	"github.com/li
+	"github.com/libsv/go-bk/wif"
+	"github.com/libsv/go-bt/v2"
+	"github.com/libsv/go-bt/v2/unlocker"
+)
+
+func main() {
+	tx := bt.NewTx()
+
+	_ = tx.From(
+		"11b476ad8e0a48fcd40807a111a050af51114877e09283bfa7f3505081a1819d",
+		0,
+		"76a914eb0bd5edba389198e73f8efabddfc61666969ff788ac6a0568656c6c6f",
+		1500,
+	)
+
+	_ = tx.PayToAddress("1NRoySJ9Lvby6DuE2UQYnyT67AASwNZxGb", 1000)
+
+	decodedWif, _ := wif.DecodeWIF("KznvCNc6Yf4iztSThoMH6oHWzH9EgjfodKxmeuUGPq5DEX5maspS")
+
+	if err := tx.FillAllInputs(context.Background(), &unlocker.Getter{PrivateKey: decodedWif.PrivKey}); err != nil {
+		log.Fatal(err.Error())
+	}
+	log.Printf("tx: %s\n"
