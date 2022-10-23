6be5b65cdc70b812356e617cb3df7059cd7147d7
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-10-23 21:42:38 +0000

    Sonar fixes

--- examples/read_txs_from_block/read_txs_from_block.go
+++ examples/read_txs_from_block/read_txs_from_block.go
@@ -12,4 +12,27 @@ import (
 // In this example, all txs from a block are being read in via chunking, so at no point
 // does the entire block have to be held in memory, and instead can be streamed.
 //
-// We represent the block by interactively readin
+// We represent the block by interactively reading a file, however it could be any data
+// stream that satisfies the io.Reader interface.
+
+func main() {
+	// Open file container block data.
+	f, err := data.TxBinData.Open("block.bin")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	// Create buffered reader for this file.
+	r := bufio.NewReader(f)
+
+	// Read file header. This step is specific to file reading and
+	// may need omitted or modified for other implementations.
+	_, err = io.ReadFull(f, make([]byte, 80))
+	if err != nil {
+		panic(err)
+	}
+
+	txs := bt.Txs{}
+	if _, err = txs.ReadFrom(r); err != nil {
+		panic(er
