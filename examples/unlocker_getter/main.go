21dc1e081abd99066f8a7f4f727673d0dbfaba8e
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-10-27 09:20:56 +0000

    Update cn.md

--- examples/unlocker_getter/main.go
+++ examples/unlocker_getter/main.go
@@ -12,4 +12,13 @@ import (
 // This example gives a simple in-memory based example of how to implement and use a `bt.UnlockerGetter`
 // using derivated public/private keys.
 //
-// The basic idea is, we have accounts, each with a master private key. If someone would like to
+// The basic idea is, we have accounts, each with a master private key. If someone would like to send money
+// to an account, they request "destinations" from the account. These destinations are added to the
+// tx, which is then ultimately broadcast.
+//
+// A destination in this example is simply a P2PKH locking script, however, the PK Hash will be unique
+// on each call as under the hood, the account is deriving a new private/public key pair from its master
+// key, creating a P2PKH script from that this pair, and storing the value used to derive this private/public
+// key pair against the P2PKH script that it produced.
+//
+// When an account wishes 
