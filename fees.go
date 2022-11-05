51f862014c5210fb5ee6b9403fd696353010e62f
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-11-05 18:55:57 +0000

    f2fs: remove the ugly pointer conversion

--- fees.go
+++ fees.go
@@ -11,3 +11,24 @@ import (
 // type of fee is used depending on
 // the type of tx data (eg: standard
 // bytes or data bytes).
+type FeeType string
+
+const (
+	// FeeTypeStandard is the fee type for standard tx parts
+	FeeTypeStandard FeeType = "standard"
+
+	// FeeTypeData is the fee type for data tx parts
+	FeeTypeData FeeType = "data"
+)
+
+// FeeQuotes contains a list of miners and the current fees for each miner as well as their expiry.
+//
+// This can be used when getting fees from multiple miners, and you want to use the cheapest for example.
+//
+// Usage setup should be calling NewFeeQuotes(minerName).
+type FeeQuotes struct {
+	mu     sync.RWMutex
+	quotes map[string]*FeeQuote
+}
+
+// NewFeeQuotes will set u
