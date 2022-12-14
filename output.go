fb4c61bab63831f44beedf051d3bdfb15ad8b93d
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-12-14 19:16:12 +0000

    'updates'

--- output.go
+++ output.go
@@ -6,4 +6,15 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/libsv/go-bt/v2/b
+	"github.com/libsv/go-bt/v2/bscript"
+	"github.com/pkg/errors"
+)
+
+/*
+General format (inside a block) of each output of a transaction - Txout
+Field	                        Description	                                Size
+-----------------------------------------------------------------------------------------------------
+value                         non-negative integer giving the number of   8 bytes
+                              Satoshis(BTC/10^8) to be transferred
+Txout-script length           non-negative integer                        1 - 9 bytes VI = VarInt
+Txout-script / scriptPub
