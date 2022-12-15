69e4c5331fd5831285774a7875e1fb6c22711cb3
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2022-12-15 07:23:12 +0000

    #i101955# work in progress of a configmgr reimplementation, continued (for now in an extra module "configmgr2")

--- output.go
+++ output.go
@@ -17,4 +17,27 @@ Field	                        Description	                                Size
 value                         non-negative integer giving the number of   8 bytes
                               Satoshis(BTC/10^8) to be transferred
 Txout-script length           non-negative integer                        1 - 9 bytes VI = VarInt
-Txout-script / scriptPub
+Txout-script / scriptPubKey   Script                                      <out-script length>-many bytes
+(lockingScript)
+
+*/
+
+// Output is a representation of a transaction output
+type Output struct {
+	Satoshis      uint64
+	LockingScript *bscript.Script
+}
+
+// ReadFrom reads from the `io.Reader` into the `bt.Output`.
+func (o *Output) ReadFrom(r io.Reader) (int64, error) {
+	*o = Output{}
+	var bytesRead int64
+
+	satoshis := make([]byte, 8)
+	n, err := io.ReadFull(r, satoshis)
+	bytesRead += int64(n)
+	if err != nil {
+		return bytesRead, errors.Wrapf(err, "satoshis(8): got %d bytes", n)
+	}
+
+	var l VarIn
