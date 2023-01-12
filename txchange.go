74768affb41220ac8f81238f4c8916fc0152a346
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2023-01-12 08:03:33 +0000

    metrics

--- txchange.go
+++ txchange.go
@@ -11,4 +11,25 @@ const (
 
 // ChangeToAddress calculates the amount of fees needed to cover the transaction
 // and adds the leftover change in a new P2PKH output using the address provided.
-func (tx *Tx) Chang
+func (tx *Tx) ChangeToAddress(addr string, f *FeeQuote) error {
+	s, err := bscript.NewP2PKHFromAddress(addr)
+	if err != nil {
+		return err
+	}
+
+	return tx.Change(s, f)
+}
+
+// Change calculates the amount of fees needed to cover the transaction
+//  and adds the leftover change in a new output using the script provided.
+func (tx *Tx) Change(s *bscript.Script, f *FeeQuote) error {
+	if _, _, err := tx.change(f, &changeOutput{
+		lockingScript: s,
+		newOutput:     true,
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ChangeToExistingOutput will calculate fees and add them to a
