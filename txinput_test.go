3071fe4faa03ed7bc7caaad61e208fc015da250c
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2023-01-18 16:26:07 +0000

    fixed jslint warnings

--- txinput_test.go
+++ txinput_test.go
@@ -9,4 +9,18 @@ import (
 
 	. "github.com/libsv/go-bk/wif"
 	"github.com/libsv/go-bt/v2"
-	"github.com/li
+	"github.com/libsv/go-bt/v2/bscript"
+	"github.com/libsv/go-bt/v2/sighash"
+	"github.com/libsv/go-bt/v2/unlocker"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddInputFromTx(t *testing.T) {
+	pubkey1, _ := hex.DecodeString("0280f642908697e8068c2e921bd998d6c2b90553064656f91b9cb9e98f443aac30")
+	pubkey2, _ := hex.DecodeString("02434dc3db4281c0895d7a126bb266e7648caca7d0e2e487bc41f954722d4ee397")
+
+	prvTx := bt.NewTx()
+	err := prvTx.AddP2PKHOutputFromPubKeyBytes(pubkey1, uint64(100000))
+	assert.NoError(t, err)
+	err = prvTx.AddP2PKHOutputFromPubKeyBytes(pubkey1, uint64(100000))
+	assert.NoEr
