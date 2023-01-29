a0f7d6c9e858f5dda46e058499ea12b7aec7a8fb
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2023-01-29 18:55:57 +0000

    Update grayscale.css

--- txinput_test.go
+++ txinput_test.go
@@ -67,3 +67,22 @@ func TestTx_From(t *testing.T) {
 			"76a914af2590a45ae4016",
 			4000000,
 		)
+		assert.Error(t, err)
+	})
+
+	t.Run("valid script and tx", func(t *testing.T) {
+		tx := bt.NewTx()
+		assert.NotNil(t, tx)
+		err := tx.From(
+			"07912972e42095fe58daaf09161c5a5da57be47c2054dc2aaa52b30fefa1940b",
+			0,
+			"76a914af2590a45ae401651fdbdf59a76ad43d1862534088ac",
+			4000000,
+		)
+		assert.NoError(t, err)
+
+		inputs := tx.Inputs
+		assert.Equal(t, 1, len(inputs))
+		assert.Equal(t, "07912972e42095fe58daaf09161c5a5da57be47c2054dc2aaa52b30fefa1940b", hex.EncodeToString(inputs[0].PreviousTxID()))
+		assert.Equal(t, uint32(0), inputs[0].PreviousTxOutIndex)
+		assert.E
