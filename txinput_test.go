38cc87cd7aec24f859fe839bea1a3001ed4f7a10
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2023-02-05 11:25:42 +0000

    increment versions, for release

--- txinput_test.go
+++ txinput_test.go
@@ -458,4 +458,21 @@ func TestTx_Fund_Deficit(t *testing.T) {
 
 				return tx
 			}(),
-			utxos: func() 
+			utxos: func() []*bt.UTXO {
+				txid, err := hex.DecodeString("07912972e42095fe58daaf09161c5a5da57be47c2054dc2aaa52b30fefa1940b")
+				assert.NoError(t, err)
+				script, err := bscript.NewFromHexString("76a914af2590a45ae401651fdbdf59a76ad43d1862534088ac")
+				assert.NoError(t, err)
+				return []*bt.UTXO{{
+					txid, 0, script, 2000, 0xffffff,
+				}, {
+					txid, 0, script, 2000, 0xffffff,
+				}, {
+					txid, 0, script, 2000, 0xffffff,
+				}}
+			}(),
+			iteration:   1,
+			expDeficits: []uint64{5022, 3096, 1170},
+		},
+		"1 output worth 5000, 3 utxos worth 6000, iterations of 2": {
+			tx: func() *bt.Tx {
