84a5de333e4af1fe8dd6b7be565bbb78a0a71f70
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2023-02-10 16:26:34 +0000

    NR-325 : Improve scalability on multiple process machines

--- txinput_test.go
+++ txinput_test.go
@@ -637,4 +637,22 @@ func TestTx_FillInput(t *testing.T) {
 
 				return &unlocker.Simple{PrivateKey: wif.PrivKey}
 			}(),
-			expHex: "01000000010b94a1ef0fb352aa2adc54207ce47ba55d5a1c1609afda58fe9520e472299107000000006a473044022049ee0c0f26c00e6a6b3af5990fc8296c66eab3e3e42ab075069b89b1be6fefec02206079e49dd8c9e1117ef06fbe99714d822620b1f0f5d19f32a1128f5d29b7c3c4412102c8803fdd437d902f08e3c2344cb33065c99d7c99982
+			expHex: "01000000010b94a1ef0fb352aa2adc54207ce47ba55d5a1c1609afda58fe9520e472299107000000006a473044022049ee0c0f26c00e6a6b3af5990fc8296c66eab3e3e42ab075069b89b1be6fefec02206079e49dd8c9e1117ef06fbe99714d822620b1f0f5d19f32a1128f5d29b7c3c4412102c8803fdd437d902f08e3c2344cb33065c99d7c99982018ff9f7219c3dd352ff0ffffffff01a0083d00000000001976a914af2590a45ae401651fdbdf59a76ad43d1862534088ac00000000",
+		},
+		"no unlocker errors": {
+			inputIdx: 0,
+			shf:      sighash.AllForkID,
+			expErr:   bt.ErrNoUnlocker,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			tx := bt.NewTx()
+			assert.NoError(t, tx.From(
+				"07912972e42095fe58daaf09161c5a5da57be47c2054dc2aaa52b30fefa1940b",
+				0,
+				"76a914af2590a45ae401651fdbdf59a76ad43d1862534088ac",
+				4000000,
+			))
+			assert.NoError(t, tx.ChangeToAddress("mwV3YgnowbJJB3LcyCuqiK
