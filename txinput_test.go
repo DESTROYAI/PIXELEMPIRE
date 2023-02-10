9f6780524e4ee70fa255ec3e339991622e461849
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2023-02-10 15:28:27 +0000

    SCSI: sd: Fix potential out-of-bounds access

--- txinput_test.go
+++ txinput_test.go
@@ -628,3 +628,13 @@ func TestTx_FillInput(t *testing.T) {
 			}(),
 			expHex: "01000000010b94a1ef0fb352aa2adc54207ce47ba55d5a1c1609afda58fe9520e472299107000000006a473044022049ee0c0f26c00e6a6b3af5990fc8296c66eab3e3e42ab075069b89b1be6fefec02206079e49dd8c9e1117ef06fbe99714d822620b1f0f5d19f32a1128f5d29b7c3c4412102c8803fdd437d902f08e3c2344cb33065c99d7c99982018ff9f7219c3dd352ff0ffffffff01a0083d00000000001976a914af2590a45ae401651fdbdf59a76ad43d1862534088ac00000000",
 		},
+		"sighash all is used as default": {
+			inputIdx: 0,
+			unlocker: func() bt.Unlocker {
+				var wif *WIF
+				wif, err := DecodeWIF("L3MhnEn1pLWcggeYLk9jdkvA2wUK1iWwwrGkBbgQRqv6HPCdRxuw")
+				assert.NoError(t, err)
+
+				return &unlocker.Simple{PrivateKey: wif.PrivKey}
+			}(),
+			expHex: "01000000010b94a1ef0fb352aa2adc54207ce47ba55d5a1c1609afda58fe9520e472299107000000006a473044022049ee0c0f26c00e6a6b3af5990fc8296c66eab3e3e42ab075069b89b1be6fefec02206079e49dd8c9e1117ef06fbe99714d822620b1f0f5d19f32a1128f5d29b7c3c4412102c8803fdd437d902f08e3c2344cb33065c99d7c99982
