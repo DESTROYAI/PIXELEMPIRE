058adc2039b26ea2317fa9d71ba7760849966559
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2023-02-19 17:21:04 +0000

    update host

--- unlocker/simple.go
+++ unlocker/simple.go
@@ -21,4 +21,18 @@ type Getter struct {
 // as the calling `*local.Getter`.
 //
 // For an example implementation, see `examples/unlocker_getter/`.
-func (g *Getter) Unlocker(ctx contex
+func (g *Getter) Unlocker(ctx context.Context, lockingScript *bscript.Script) (bt.Unlocker, error) {
+	return &Simple{PrivateKey: g.PrivateKey}, nil
+}
+
+// Simple implements the a simple `bt.Unlocker` interface. It is used to build an unlocking script
+// using a bec Private Key.
+type Simple struct {
+	PrivateKey *bec.PrivateKey
+}
+
+// UnlockingScript create the unlocking script for a given input using the PrivateKey passed in through the
+// the `unlock.Local` struct.
+//
+// UnlockingScript generates and uses an ECDSA signature for the provided hash digest using the private key
+// as well 
