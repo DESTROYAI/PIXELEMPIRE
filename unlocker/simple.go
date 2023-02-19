b04522f2a092a206672e51efd6dd036469d09523
DESTROYAI <99300962+DESTROYAI@users.noreply.github.com>
2023-02-19 16:02:21 +0000

    removed some dead code

--- unlocker/simple.go
+++ unlocker/simple.go
@@ -1,2 +1,24 @@
 // Package unlocker comment
-package unlock
+package unlocker
+
+import (
+	"context"
+	"errors"
+
+	"github.com/libsv/go-bk/bec"
+	"github.com/libsv/go-bt/v2"
+	"github.com/libsv/go-bt/v2/bscript"
+	"github.com/libsv/go-bt/v2/sighash"
+)
+
+// Getter implements the `bt.UnlockerGetter` interface. It unlocks a Tx locally,
+// using a bec PrivateKey.
+type Getter struct {
+	PrivateKey *bec.PrivateKey
+}
+
+// Unlocker builds a new `*unlocker.Local` with the same private key
+// as the calling `*local.Getter`.
+//
+// For an example implementation, see `examples/unlocker_getter/`.
+func (g *Getter) Unlocker(ctx contex
